f4ccfd30b439d96c80ec1f9994f35c6ecaa01a98
NaraeJang <j.naaaarae@gmail.com>
2023-10-05 14:18:53 -0400

    submenu JSX

--- src/Submenu.jsx
+++ src/Submenu.jsx
@@ -1,4 +1,31 @@
+import { useGlobalContext } from "./context";
+import sublinks from "./data";
+
 const Submenu = () => {
-  return <h2>Submenu</h2>;
+  const { pageId, setPageId } = useGlobalContext();
+  const currentPage = sublinks.find((item) => item.pageId === pageId);
+  console.log(currentPage);
+  return (
+    <div className="submenu show-submenu">
+      <h5>{currentPage?.page}</h5>
+      <div
+        className="submenu-links"
+        style={{
+          gridTemplateColumns:
+            currentPage?.links?.length > 3 ? "1fr 1fr" : "1fr",
+        }}>
+        {currentPage?.links?.map((link) => {
+          const { id, label, icon, url } = link;
+
+          return (
+            <a href={url} key={id}>
+              {icon}
+              {label}
+            </a>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 export default Submenu;
